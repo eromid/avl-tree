fc2c60efa934fdfc449a481ad228a8d3e39daff0
Olly <eromid@users.noreply.github.com>
2017-09-29 12:46:21 +0100

    Initial commit of tests.

---
+++ avltree-test-helper.h
@@ -0,0 +1,90 @@
+/*
+optional.h
+Copyright (c) Eromid (Olly) 2017
+
+Friend of avltree class which contains functions only used in testing.
+*/
+
+#ifndef AVL_TEST_HELPER_H
+#define AVL_TEST_HELPER_H
+
+#include <algorithm>
+#include <cmath>
+
+using std::max;
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+template<typename K, typename V>
+class test_helper
+{
+public:
+  static unsigned int count_nodes(const avltree<K, V>& tree)
+  {
+    if (tree.root)
+      return count_descendants(tree.root);
+    else
+      return 0u;
+  }
+
+  static unsigned int count_descendants(const shared_ptr<typename avltree<K, V>::node> node)
+  {
+    unsigned int left_count = node->left_child ? count_descendants(node->left_child) : 0;
+    unsigned int right_count = node->right_child ? count_descendants(node->right_child) : 0;
+    return 1u + left_count + right_count;
+  }
+
+  static unsigned int subtree_height(const shared_ptr<typename avltree<K, V>::node> node)
+  {
+    if (!node) return 0u;  // no subtree root -> height = 0.
+    unsigned int left_height, right_height;
+    left_height = right_height = 0u;
+    left_height = subtree_height(node->left_child);
+    right_height = subtree_height(node->right_child);
+    return 1 + max(left_height, right_height);
+  }
+
+  static bool is_avl(const avltree<K, V>& tree)
+  {
+    return is_avl(tree.root);
+  }
+
+  static bool is_avl(const shared_ptr<typename avltree<K, V>::node> root_node)
+  {
+    if (!root_node) return true;  // Base case; an empty tree is always AVL.
+    bool left_is_avl, right_is_avl;
+    unsigned int left_height, right_height;
+    left_is_avl = is_avl(root_node->left_child);
+    left_height = subtree_height(root_node->left_child);
+    right_is_avl = is_avl(root_node->right_child);
+    right_height = subtree_height(root_node->right_child);
+    if (root_node->parent.lock() == nullptr)
+    {
+      cout << "left subtree: " << left_height << " | right subtree: " << right_height << endl;
+      cout << "left_is_avl: " << left_is_avl << " | right_is_avl: " << right_is_avl << endl;
+    }
+    return left_is_avl && right_is_avl && (abs(left_height - right_height) <= 1u);
+  }
+
+  static bool valid_balance_factors(const avltree<K, V>& tree)
+  {
+    return valid_balance_factors(tree.root);
+  }
+
+
+  static bool valid_balance_factors(const shared_ptr<typename avltree<K, V>::node> node)
+  {
+    if (!node) return true;
+    cout << "left_height: " << subtree_height(node->left_child) << " | right_height: "
+         << subtree_height(node->right_child) << " | balance_factor: " << static_cast<int>(node->balance_factor) << endl;
+    return (subtree_height(node->left_child) - subtree_height(node->right_child) == node->balance_factor)
+            && valid_balance_factors(node->left_child) && valid_balance_factors(node->right_child);
+  }
+
+private:
+  test_helper() {} // instantiation not needed (or allowed)
+};
+
+#endif
---
+++ test-avl.cpp
@@ -0,0 +1,134 @@
+/*
+optional.h
+Copyright (c) Eromid (Olly) 2017
+
+Unit tests for AVL tree implementation in avltree.h.
+*/
+
+#include <assert.h>
+#include "avltree.h"
+#include "avltree-test-helper.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::left;
+using std::right;
+
+#include <iomanip>
+using std::setw;
+using std::setfill;
+
+using tests = test_helper<int, double>;
+
+#define TEST_CASE(fn)\
+cout << "================================================================================" << endl;\
+cout << "    Running " << #fn << "...";                                                            \
+fn();                                                                                              \
+cout << " Complete. " << endl;                                                                     \
+cout << "================================================================================" << endl;
+
+void test_empty_tree()
+{
+  avltree<int, double> tree;
+  assert(tests::is_avl(tree));            // Tree should be an AVL tree
+  assert(tests::count_nodes(tree) == 0);  // Empty tree should have 0 nodes.
+  tree.remove(123);                       // Removing non-existing key should have no effect.
+  assert(tests::count_nodes(tree) == 0);  // Empty tree should still have 0 nodes.
+  assert(tests::is_avl(tree));            // Tree should be an AVL tree still
+  assert(tests::valid_balance_factors(tree)); // Are all balance factors correct (none)
+}
+
+void test_single_node()
+{
+  const double value = 42.0;
+  const int key = 1;
+  avltree<int, double> tree;
+  tree.insert(key, value);
+
+  // After insertion of single node:
+  assert(tests::count_nodes(tree) == 1);      // Empty tree should have 0 nodes.
+  assert(tests::is_avl(tree));                // Tree should be an AVL tree still
+  tree.remove(123);                           // Removing non-existing key should have no effect.
+  assert(tests::count_nodes(tree) == 1);      // Tree should now have one node
+  assert(tests::is_avl(tree));                // Tree should be an AVL tree still
+  assert(tree.get(key).has_value() == true);  // query should return a value
+  assert(tree.get(key).value() == value);     // value should be what we put in
+  assert(tests::valid_balance_factors(tree)); // Are all balance factors correct
+
+  // after removing single node:
+  tree.remove(key);
+  assert(tests::count_nodes(tree) == 0);      // Empty tree should still have 0 nodes.
+  assert(tests::is_avl(tree));    
+  assert(tree.get(key).has_value() == false);      // query should return a value
+  assert(tests::valid_balance_factors(tree)); // Are all balance factors correct
+
+}
+
+void test_multiple_right_insertions()
+{
+  avltree<int, double> tree;
+  const static int n_insertions = 100;
+  for (unsigned int i = 1; i <= n_insertions; ++i)
+  {
+    tree.insert(i, static_cast<double>(i));   // insert increasing numbers
+    assert(tests::is_avl(tree));              // check tree remains AVL
+    assert(tests::count_nodes(tree) == i);          // Check the node made is in
+    assert(tests::valid_balance_factors(tree)); // Are all balance factors correct
+  }
+  for (int i = 1; i < n_insertions; ++i)
+  {
+    assert(tree.get(i).has_value() == true);  // query should return a value
+    assert(tree.get(i).value() == static_cast<double>(i));     // value should be what we put in
+  }
+}
+
+void test_multiple_left_insertions()
+{
+  avltree<int, double> tree;
+  const static int n_insertions = 100;
+  for (unsigned int i = n_insertions; i >= 1; --i)
+  {
+    tree.insert(i, static_cast<double>(i));   // insert increasing numbers
+    assert(tests::is_avl(tree));              // check tree remains AVL
+    assert(tests::count_nodes(tree) == n_insertions - (i -1));          // Check the node made is in
+    assert(tests::valid_balance_factors(tree)); // Are all balance factors correct
+  }
+  for (int i = 1; i < n_insertions; ++i)
+  {
+    assert(tree.get(i).has_value() == true);  // query should return a value
+    assert(tree.get(i).value() == static_cast<double>(i));     // value should be what we put in
+  }
+}
+
+void test_right_left_rotation()
+{
+  avltree<int, double> tree;
+  tree.insert(10, 10.0);      // Root
+  tree.insert(20, 20.0);      // Right child of root
+  tree.insert(15, 15.0);      // left child of right child of root -> triggers RL rotation
+  assert(tests::is_avl(tree));
+  assert(tests::valid_balance_factors(tree));
+
+}
+
+void test_left_right_rotation()
+{
+  avltree<int, double> tree;
+  tree.insert(20, 20.0);        // Root node
+  tree.insert(10, 10.0);        // Left child
+  tree.insert(15, 15.0);        // Right child of left child -> triggers LR rotation
+  assert(tests::is_avl(tree));
+  assert(tests::valid_balance_factors(tree));
+}
+
+int main()
+{
+  TEST_CASE(test_empty_tree);
+  TEST_CASE(test_single_node);
+  TEST_CASE(test_multiple_right_insertions);
+  TEST_CASE(test_multiple_left_insertions);
+  TEST_CASE(test_right_left_rotation);
+  TEST_CASE(test_left_right_rotation);
+  return 0;
+}
